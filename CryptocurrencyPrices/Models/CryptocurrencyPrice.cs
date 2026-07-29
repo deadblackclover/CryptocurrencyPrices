dfe21ff185fe6ce8509a4d6818d1aa198aaf4a11
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-07-29 19:33:51 +0700

    Added CryptocurrencyPrice

---
+++ CryptocurrencyPrices/Models/CryptocurrencyPrice.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptocurrencyPrices.Models
+{
+    class CryptocurrencyPrice
+    {
+        public Cryptocurrency Cryptocurrency { get; set; }
+        public float Price { get; set; }
+    }
+}
