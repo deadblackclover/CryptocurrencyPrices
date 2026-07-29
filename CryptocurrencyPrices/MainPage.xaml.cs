39eed4884fa3070b091df00b03d75b653de98c92
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-07-29 19:44:43 +0700

    Added MainPage.xaml.cs

---
+++ CryptocurrencyPrices/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CryptocurrencyPrices.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace CryptocurrencyPrices
+{
+    public sealed partial class MainPage : Page
+    {
+        MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await ViewModel.LoadDataAsync();
+        }
+    }
+}
