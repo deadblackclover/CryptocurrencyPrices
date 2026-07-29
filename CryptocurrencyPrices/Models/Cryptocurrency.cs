5660dacb22c5d1bdb44bce312326461e93eb89b1
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-07-29 19:39:09 +0700

    Added CryptocurrencyService

---
+++ CryptocurrencyPrices/Models/Cryptocurrency.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptocurrencyPrices.Models
+{
+    class Cryptocurrency
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Ticker { get; set; }
+    }
+}
---
+++ CryptocurrencyPrices/Services/CryptocurrencyService.cs
@@ -0,0 +1,28 @@
+﻿using CryptocurrencyPrices.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace CryptocurrencyPrices.Services
+{
+    class CryptocurrencyService
+    {
+        const string PATH = "ms-appx:///Assets/data/cryptocurrency.json";
+
+        public async Task<List<Cryptocurrency>> LoadCurrencies()
+        {
+            var jsonString = await LoadJsonFromFileAsync();
+            var currencies = JsonConvert.DeserializeObject<List<Cryptocurrency>>(jsonString);
+            return currencies;
+        }
+
+        private async Task<string> LoadJsonFromFileAsync()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(PATH));
+            string json = await FileIO.ReadTextAsync(file);
+            return json;
+        }
+    }
+}
