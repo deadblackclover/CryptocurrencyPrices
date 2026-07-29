e55d7e842a6b7851edd3759c9421ccc04913a819
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-07-29 23:35:08 +0700

    Added ImagePathConverter

---
+++ CryptocurrencyPrices/Converters/ImagePathConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace CryptocurrencyPrices.Converters
+{
+    class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var bitmapImage = new BitmapImage();
+            var uriString = CreatePath(value is string name && !string.IsNullOrEmpty(name) ? name : "Bitcoin");
+            bitmapImage.UriSource = new Uri(uriString);
+            return bitmapImage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CreatePath(string name) => $"ms-appx:///Assets/Coins/{name}.png";
+    }
+}
