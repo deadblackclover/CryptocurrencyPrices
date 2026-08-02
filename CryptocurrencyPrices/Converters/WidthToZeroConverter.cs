549272db3bd9e366b5dc87eb2d1389558559b5db
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2026-08-02 13:23:54 +0700

    Added view ticker

---
+++ CryptocurrencyPrices/Converters/WidthToZeroConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace CryptocurrencyPrices.Converters
+{
+    class WidthToZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double currentWidth && parameter is string minWidthString)
+            {
+                var minWidth = double.Parse(minWidthString);
+                return currentWidth <= minWidth ? 0.0 : double.NaN;
+            }
+
+            return 0.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
